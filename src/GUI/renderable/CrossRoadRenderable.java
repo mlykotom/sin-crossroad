86ed3965d57065bd2c518353c0d0655e64288084
Tomáš Mlynarič <mlynarict@gmail.com>
2016-11-30 01:51:41 +0100

    Adds random semaphores

--- src/GUI/renderable/CrossRoadRenderable.java
+++ src/GUI/renderable/CrossRoadRenderable.java
@@ -3,10 +3,13 @@ package GUI.renderable;
 import Map.CrossRoad;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 
 public class CrossRoadRenderable extends PlaceRenderable<CrossRoad> {
+
 
     public CrossRoadRenderable(CrossRoad place) {
         super(place);
@@ -24,18 +27,27 @@ public class CrossRoadRenderable extends PlaceRenderable<CrossRoad> {
         return getWidth(cellSize);
     }
 
+
     @Override
     public void render(Graphics2D g2D, float cellSize) {
         float realX = getRealPositionX(cellSize);
         float realY = getRealPositionY(cellSize);
+        float width = getWidth(cellSize);
+        float height = getHeight(cellSize);
 
-        Rectangle2D innerCross = new Rectangle2D.Float(
-                realX, realY,
-                getWidth(cellSize),
-                cellSize / 2
-        );
-
+        Rectangle2D innerCross = new Rectangle2D.Float(realX, realY, width, height);
 
         drawShape(g2D, innerCross, Color.DARK_GRAY);
+
+        for (int i = 0; i < 4; i++) {
+            int yFactor = i / 2;
+            Ellipse2D semaphore = new Ellipse2D.Float(realX + (i % 2 * width), realY + yFactor * height, width / 4, width / 4);
+            drawShape(g2D, semaphore, calculateSemaphoreColor(i));
+        }
+    }
+
+
+    private Paint calculateSemaphoreColor(int i) {
+        return new Random().nextBoolean() ? Color.RED : Color.GREEN;
     }
 }
